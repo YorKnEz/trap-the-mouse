0b62cad7a57be461ba6c2b8d584beb79dd1c4f9e
Alex Mitreanu <alexmitreanu04@gmail.com>
2023-12-28 17:28:58 +0200

    add client crate

---
+++ client/src/main.rs
@@ -0,0 +1,26 @@
+use std::net::SocketAddr;
+
+use network::{request, Type};
+
+fn main() {
+    let res: String = match request("127.0.0.1:20000", Type::Ping, &"01234567") {
+        Ok(res) => res,
+        Err(e) => return println!("couldn't request: {e:?}")
+    };
+
+    println!("received: {res}");
+
+    let addr: SocketAddr = match request("127.0.0.1:20000", Type::CreateLobby, &"") {
+        Ok(res) => res,
+        Err(e) => return println!("couldn't request: {e:?}")
+    };
+
+    println!("received: {addr:?}");
+
+    let res: String = match request(addr, Type::Ping, &"01234567") {
+        Ok(res) => res,
+        Err(e) => return println!("couldn't request: {e:?}")
+    };
+
+    println!("received: {res}");
+}
