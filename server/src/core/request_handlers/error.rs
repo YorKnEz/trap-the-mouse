cf6d214dda56e71a1e50f4e1ab94c7cf4e020dca
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-01-02 13:41:25 +0200

    add better error system for API

--- server/src/core/request_handlers/error.rs
+++ server/src/core/request_handlers/error.rs
@@ -3,7 +3,23 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum ServerError {
     #[error("internal error")]
-    Internal(#[from] anyhow::Error),
+    InternalAnyhow(#[from] anyhow::Error),
+
+    #[error("internal error")]
+    InternalIo(#[from] std::io::Error),
+
+    #[error("internal error")]
+    InternalR2d2(#[from] r2d2::Error),
+
+    #[error("internal error")]
+    InternalRusqlite(#[from] rusqlite::Error),
+
+    #[error("internal error")]
+    InternalAddrParseError(#[from] std::net::AddrParseError),
+
     #[error("{message}")]
     API { message: String },
+
+    #[error("you are not connected")]
+    APINotConnected,
 }
--- server/src/core/request_handlers/mod.rs
+++ server/src/core/request_handlers/mod.rs
@@ -9,6 +9,9 @@ mod error;
 
 use anyhow::Result;
 
+use serde::Serialize;
+
+use network::Type;
 
 pub use invalid::InvalidRequest;
 pub use ping::PingRequest;
@@ -17,6 +20,22 @@ pub use exit::ExitRequest;
 pub use lobby::*;
 pub use server::*;
 
+use error::ServerError;
+
 pub trait Request {
     fn execute(&mut self) -> Result<()>;
+}
+
+pub fn error_check<T: Serialize>(res: Result<T, ServerError>) -> Result<(Type, Vec<u8>)> {
+    Ok(match res {
+        Ok(res) => (Type::Success, bincode::serialize(&res)?),
+        Err(e) => {
+            println!("request error: {e:?}");
+            match e {
+                ServerError::API { message } => (Type::Error, bincode::serialize(&message)?),
+                ServerError::APINotConnected => (Type::Error, bincode::serialize(&"you are not connected")?),
+                _ => (Type::Error, bincode::serialize("internal error")?),
+            }
+        }
+    })
 }
